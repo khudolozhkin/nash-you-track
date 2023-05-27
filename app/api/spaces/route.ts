4c4c91a77b91246f617480db768ff2d718093eed
Alexander Khudolozhkin <ahuddmt@gmail.com>
2023-05-27 23:28:53 +0300

    feat: auth check and error throws

---
+++ app/api/spaces/route.ts
@@ -0,0 +1,79 @@
+import * as z from "zod"
+import { userSpaceSchema } from "@/lib/validations/spaces";
+import { NextResponse } from 'next/server';
+import { getCurrentUser } from '@/lib/session';
+import { db } from '@/lib/db';
+
+export async function GET() {
+  try {
+    const user = await getCurrentUser();
+    
+    if (user == undefined) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+    
+    const spaces = await db.user.findMany({
+      where: {
+        id: user?.id,
+      },
+      select: {
+        spaces: {
+          select: {
+            spaceId: true,
+            userId: true,
+            permission: true,
+            space: {
+              select: {
+                name: true
+              }
+            }
+          }
+        }
+      }
+    })
+  
+    return NextResponse.json(spaces[0]);
+  
+  } catch (error) {
+
+    return new Response(null, { status: 500 })
+  
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const user = await getCurrentUser();
+
+    if (user == undefined) {
+      return new Response("Unauthorized", { status: 403 })
+    }
+
+    const json = await request.json();
+    const body = userSpaceSchema.parse(json)
+
+    const newSpace = await db.space.create({
+      data: {
+        name: body.name
+      }
+    })
+
+    const newOwnerSpace = await db.spaceUser.create({
+      data: {
+        permission: "OWNER",
+        userId: `${user?.id}`,
+        spaceId: newSpace?.id
+      }
+    })
+    
+    return NextResponse.json({newOwnerSpace, newSpace});
+
+  } catch (error) {
+
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 })
+    }
+
+    return new Response(null, { status: 500 })
+  }
+}
