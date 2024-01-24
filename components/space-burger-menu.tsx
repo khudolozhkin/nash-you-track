fbe9cbe13f5dc272c6a1e98696f05fd5fa77931c
Alexander Khudolozhkin <ahuddmt@gmail.com>
2024-01-24 10:51:01 +0300

    fix: mobile nav

--- components/space-burger-menu.tsx
+++ components/space-burger-menu.tsx
@@ -23,7 +23,7 @@ export default function SpaceBurgerMenu({name, accessLevel, spaceId, dashboards,
           </div>
           <div className="block sm:hidden flex flex-row-reverse border-b border-border dark:border-border-dark"><Icons.burger onClick={() => {setActive(true)}} className="mr-[32px] my-2 cursor-pointer"/></div>
           {active ? <>
-            <div className="bg-brand-background container dark:bg-brand-background-dark h-full w-full absolute top-0 left-0 z-[11111]">
+            <div onClick={() => {setActive(false)}} className="bg-brand-background container dark:bg-brand-background-dark h-full w-full absolute top-0 left-0 z-[11111]">
               <div className="mb-4 mt-8 flex justify-center items-center gap-2">
                 <h1 className="whitespace-nowrap truncate font-bold text-xl md:text-2xl text-primary dark:text-primary-dark">{name}</h1>
                 <Icons.close onClick={() => {setActive(false)}} className="cursor-pointer"/>
